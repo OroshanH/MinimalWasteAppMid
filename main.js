94668dfb91299dd9333d8708f7f50a300093041e
Oro <Oroshanh@hotmail.com>
2020-10-25 21:22:40 +0100

    123

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+freezer.onclick = function(){
+    window.location.href = "freezer.html";
+
+}
+recipes.onclick = function(){
+    window.location.href = "recipes.html";
+
+}
+account.onclick = function(){
+    window.location.href = "account.html";
+
+}
+
